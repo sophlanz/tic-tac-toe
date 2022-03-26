92705bc0b50723851ad4d58c67f7cad1745fb1dc
Sophlanz <sophialanzendorf@gmail.com>
2022-03-26 11:54:00 -0400

    completed CSS and HTML. Added a modal to the JS, and a notification for a tie.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
         createGameBoard();
         
         document.addEventListener('DOMContentLoaded',()=> {
-            document.getElementById("clearBtn").addEventListener('click', clearGameBoard);
+            document.getElementById("btn").addEventListener('click', clearGameBoard);
         })
     } ;
   
@@ -27,101 +27,199 @@
 //score counters
 let p1Counter = 0;
 let p2Counter = 0;
+//checks if pq has won
 function p1winner (){
     const spaces = document.querySelectorAll("#space")
-    const p1Moves = [];
+    const p1Numbers = [];
+    function someoneWon() {
+        p1Counter ++;
+        const storedPlayers = JSON.parse(localStorage.getItem('players'));
+         const p1Name = storedPlayers[0].name
+         toggleModalName(p1Name);
+        score();
+        }
     //get P1's moves
     for(let i=0;i<9;i++) {
         //check if it has the p1 attribute and is player 1
         if(spaces[i].hasAttribute('player1') && spaces[i].getAttribute('player1') == "true") {
             //keep track of p1 chosen spaces by pushing it to the array
-            p1Moves.push(spaces[i]);
+            
+            p1Numbers.push(spaces[i].getAttribute('number'));
         } 
     }
-   
 //We have the moves, now check for a win
             //looop through p1Moves and check for 3 in a row
-            for(let i=0;i<=p1Moves.length-3;i++) {
-                for(let j=1;j<=p1Moves.length-2;j++) {
-                    for(let k=2;k<=p1Moves.length-1;k++) {
-                      //get the number of the 3rd one and subtract the 2nd one. Get the difference.
+            for(let i=0;i<=p1Numbers.length-3;i++) {
+                for(let j=1;j<=p1Numbers.length-2;j++) {
+                    for(let k=2;k<=p1Numbers.length-1;k++) {
+                        
+                 //get the number of the 3rd one and subtract the 2nd one. Get the difference.
                       //check to see if the difference also applies to the 2nd-1st.
-                     const difference1 = p1Moves[k].getAttribute('number') - p1Moves[j].getAttribute('number');
-                   const difference2 = p1Moves[j].getAttribute('number') - p1Moves[i].getAttribute('number');
-                   if (difference1 == difference2 && difference1 !==1 ) {
-                       p1Counter ++;
-                       score();
-                       //make sure if it increments by 1, that the first space chosen is the first space in the row. so the index must be 0,3,6
-                   } else if(difference1==difference2 && (p1Moves[0].getAttribute('number')== (0||3||6))) {
-                    p1Counter ++;
-                    score();
-                   }
-            }
-         }
-    }
-}
+                      const difference1 = p1Numbers[k] - p1Numbers[j];
+                      const difference2 = p1Numbers[j] - p1Numbers[i];
+                         if (difference1 == 1 && difference2 == 1) {
+                             //create set for comparison
+                                if(p1Numbers.includes("0")&& p1Numbers.includes("1")&& p1Numbers.includes("2")) {
+                                    return someoneWon();
+                                }else if (p1Numbers.includes("3")&& p1Numbers.includes("4")&& p1Numbers.includes("5")) {
+                                    return someoneWon();
+                                }else if (p1Numbers.includes("6")&& p1Numbers.includes("7")&& p1Numbers.includes("8")) {
+                                    return someoneWon();
+                                }
+                            }
+                         else if (difference1 == 2 && difference2 == 2) {
+                              if(p1Numbers.includes("2")&& p1Numbers.includes("4")&& p1Numbers.includes("6")){
+                                 return  someoneWon();
+                              }
+                             
+                         } else if (difference1 == 3 && difference2 == 3) {
+                              
+                             return someoneWon()
+ 
+                         }else if (difference1==4 && difference2==4) {
+                              //create set for comparison
+                             return someoneWon();
+                         } 
+                      }
+                     }}
+                 };
+//checks if p2 has won
 function p2winner () {
     const spaces = document.querySelectorAll("#space")
-    const p2Moves = [];
+    const p2Numbers = [];
+ function someoneWon() {
+    p2Counter ++;
+    const storedPlayers = JSON.parse(localStorage.getItem('players'));
+     const p2Name = storedPlayers[1].name
+     toggleModalName(p2Name);
+    score();
+    }
      //get P2's moves
      for(let i=0;i<spaces.length;i++) {
         //check if it has the p1 attribute and is player 1
         if (spaces[i].hasAttribute('player1') && spaces[i].getAttribute('player1') !== "true"){
             //keep track of p2 chosen spaces by pushing it to the array
-            p2Moves.push(spaces[i]);
+            p2Numbers.push(spaces[i].getAttribute('number'));
         }
     }
             //loop through p2Moves and check for 3 in a row
-            for(let i=0; i<=p2Moves.length-3;i++) {
-                for(let j=1;j<=p2Moves.length-2;j++) {
-                    for(let k=2;k<=p2Moves.length-1;k++) {
-                      //get the number of the 3rd one and subtract the 2nd one. Get the difference.
+            for(let i=0; i<=p2Numbers.length-3;i++) {
+                for(let j=1;j<=p2Numbers.length-2;j++) {
+                    for(let k=2;k<=p2Numbers.length-1;k++) {
+                         //get the number of the 3rd one and subtract the 2nd one. Get the difference.
                       //check to see if the difference also applies to the 2nd-1st.
-                     const difference1 = p2Moves[k].getAttribute('number') - p2Moves[j].getAttribute('number');
-                   const difference2 = p2Moves[j].getAttribute('number') - p2Moves[i].getAttribute('number');
-                   if (difference1 == difference2 && difference1 !==1 ) {
-                       p2Counter ++;
-                       score();
-                   } else if (difference1==1 && difference2 ==1 && (p2Moves[0].getAttribute('number')  == (0||3||6))){
-                    p2Counter ++;
-                    const storedPlayers = JSON.parse(localStorage.getItem('players'));
-                    const p2Name = storedPlayers[1].name
-                    toggleModalName(p2Name);
-                    score();
+                     const difference1 = p2Numbers[k] - p2Numbers[j];
+                     const difference2 = p2Numbers[j] - p2Numbers[i];
+                        
+                     if (difference1 == 1 && difference2 == 1) {
+                            //create set for comparison
+                            if(p2Numbers.includes("0")&& p2Numbers.includes("1")&& p2Numbers.includes("2")) {
+                                return someoneWon();
+                            }else if (p2Numbers.includes("3")&& p2Numbers.includes("4")&& p2Numbers.includes("5")) {
+                                return someoneWon();
+                            }else if (p2Numbers.includes("6")&& p2Numbers.includes("7")&& p2Numbers.includes("8")) {
+                                return someoneWon();
+                            }
+                            } else if (difference1 == 2 && difference2 == 2) {
+                             //create set for comparison
+                             if(p2Numbers.includes("2")&& p2Numbers.includes("4")&& p2Numbers.includes("6")){
+                                return someoneWon();
+                            }
+                            
+                        } else if (difference1 == 3 && difference2 == 3) {
+                            //no need to compare
+                                 return someoneWon();
+                             
+
+                        }else if (difference1==4 && difference2==4) {
+                          //no need to compare   
+                            return someoneWon();
+                        } }
+                     }
                     }
-                   }
-                 }
-             }
-         };
+                };
 let turn = 0;//player 1, turn=1 player2
 let turnCounter1 = 0;
 let turnCounter2 =0;
+upDown();
+//updown animation for player turn
+function upDown  () {
+    if(turn == 0 ){
+    var style = document.createElement('style');
+    //create moving up and down for the specific player
+    var keyFrames = '\
+    @keyframes MoveUpDown {\
+        0%, 100% {\
+          transform: translateY(0px);\
+        }\
+        50% {\
+          transform: translateY(-10px);\
+        }\
+    }';
+    style.innerHTML = keyFrames;
+    
+    document.getElementById('p1Card').appendChild(style);
+    document.getElementById('p1Card').style.animation = 'MoveUpDown 1s linear infinite';
+    if(turnCounter2 > 0){
+        
+        document.getElementById('p2Card').style.animation= "none ";
+    }
+    } else if(turn == 1 ) {
+        var style = document.createElement('style');
+        //create moving up and down for the specific player
+        var keyFrames = '\
+        @keyframes MoveUpDown {\
+            0%, 100% {\
+              transform: translateY(0px);\
+            }\
+            50% {\
+              transform: translateY(-10px);\
+            }\
+        }';
+        
+        style.innerHTML = keyFrames;
+        document.getElementById('p2Card').appendChild(style);
+        document.getElementById('p2Card').style.animation = 'MoveUpDown 1s linear infinite';
+        if(turnCounter1 > 0){
+            
+            document.getElementById('p1Card').style.animation = " none";
+        }
+    }}
 //event listener on each space. Will invoke the winner function. 
 function makeAMove (){
     //checking to see there's been at least 3 turns
     if(turn == 0){
-            this.innerText = "X";
+            this.innerHTML = "<img src=\"images/X.svg\">"; 
             //remove listener so the move can't be changed.
             this.removeEventListener('click', makeAMove);
             //monitor whether the space has been selected and by which player
             this.setAttribute('player1', 'true');
             turnCounter1 ++
+            
             if (turnCounter1 > 2){
                 p1winner();
-            }
+            } 
+            
             
             turn++
+            upDown();
     } else { 
-        this.innerText = "O";
+        this.innerHTML = "<img src=\"images/O.svg\">";
+    
         //remove listener so the move can't be changed.
         this.removeEventListener('click', makeAMove);
         //monitor whether the space has been selected and by who
             this.setAttribute('player1', 'false');
             turnCounter2 ++
+            
             if (turnCounter2 > 2){
                 p2winner();
             }
         turn--
+        upDown();
+    }
+    if(turnCounter1 == 5 || turnCounter2 == 5 ) {
+     toggleModalTie();
     }
     };
 //adds spaces to gameboard, add's event listener for makeAMove
@@ -178,25 +276,24 @@ function clearGameBoard (){
      //create player 1 user object
      let player1 = {
          name:document.querySelector('#p1Name').value,
-         marker: document.querySelector('#p1Marker').value
-     }
+         marker: "X"
+     };
      //create player 2 user object
      let player2 = {
          name:document.querySelector('#p2Name').value,
-         marker: document.querySelector('#p2Marker').value
-     }
+         marker: "O"
+     };
      //add send data function to be used for display
      player1.sendData = function (){
-        document.getElementById('display1')
+        document.getElementById("p1Display")
         .innerText= player1.name;
         
-     }
+     };
      player2.sendData = function () {
-        document.getElementById('display2')
+        document.getElementById("p2Display")
         .innerText = player2.name;
 
-     }
-   
+     };
     //push to player array;
      players.push(player1,player2);
      //save to local storage
@@ -206,23 +303,21 @@ function clearGameBoard (){
      //send added notification
      console.warn("added", {players});
     //for display purposes
+    window.location = "index.html";
     player1.sendData();
     player2.sendData();
-    window.location = "index.html";
+    
     
     //player1.sendDataDisplay();
     //player2.sendDataDisplay();
 
-
-    
-    
  };
 //displays score on screen, resets turn counter to zero
 function score () {
   const p1Score =  document.querySelector("#p1score");
   const p2Score =  document.querySelector("#p2score");
-  p1Score.innerText = p1Counter;
-  p2Score.innerText= p2Counter;
+  p1Score.innerText = `Wins: ${p1Counter}`;
+  p2Score.innerText= `Wins: ${p2Counter}`;
   turnCounter1 == 0;
   turnCounter2 ==0;
 
@@ -241,6 +336,13 @@ function toggleModal (){
     modal.classList.toggle('show-modal');
     
    };
+function toggleModalTie (){
+    const modalDisplay = document.querySelector("#pop-up");
+    const modal = document.querySelector(".modal");
+    modal.classList.toggle('show-modal');
+    modalDisplay.innerText = ("It's a tie! Play again!");
+    clearGameBoard();
+   };
 
 (function modal(){
     if(window.location.href.indexOf('index')!==-1){
@@ -249,7 +351,7 @@ function toggleModal (){
         close.addEventListener('click',toggleModal);
         //click anywhere on the screen to click out of winner. 
         window.addEventListener('click',closeOnClick);
-    }
+    };
     function closeOnClick (event) {
         const modal = document.querySelector(".modal")
         if(event.target == modal){
@@ -257,9 +359,6 @@ function toggleModal (){
         }
     };
 })();
-//click on window and close modal
-
-
 
 
 
